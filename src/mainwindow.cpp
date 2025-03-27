c1e356932ea8cd86550f6944412767b4a0711704
me_me <ichshlanger2@gmail.com>
2025-03-28 01:30:33 +0300

    new: label and resize added

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -1,4 +1,12 @@
 #include "mainwindow.h"
+#include <QLabel>
 
-MainWindow::MainWindow(QWidget *parent): QMainWindow(parent) {}
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent) {
+  setWindowTitle("neviewer");
+  resize(1280, 720);
+  
+  QLabel* label = new QLabel("Simple label", this);
+  }
+
 MainWindow::~MainWindow() {}
