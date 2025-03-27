28fa0d02be0942f700226ffce9d3b993f888947f
me_me <ichshlanger2@gmail.com>
2025-03-27 22:23:44 +0300

    new: main window added

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,10 @@
-#include <iostream>
+#include "mainwindow.h"
+
+#include <QApplication>
 
 int main(int argc, char** argv) {
-  return 0;
+  QApplication a(argc, argv);
+  MainWindow w;
+  w.show();
+  return a.exec();
 }
---
+++ src/mainwindow.cpp
@@ -0,0 +1,4 @@
+#include "mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent): QMainWindow(parent) {}
+MainWindow::~MainWindow() {}
