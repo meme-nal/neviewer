ffe6e8bd3bde3a53ed4480a5467bcc4e8009501b
me_me <ichshlanger2@gmail.com>
2025-03-29 03:23:20 +0300

    new: base elements added

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -1,12 +1,30 @@
 #include "mainwindow.h"
 #include <QLabel>
+#include <QPushButton>
+#include <QMenu>
+#include <QLineEdit>
+#include <QHBoxLayout>
+#include <QVBoxLayout>
+#include <QGridLayout>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent) {
   setWindowTitle("neviewer");
-  resize(1280, 720);
+  resize(1280, 720); // 1280 x 720
   
   QLabel* label = new QLabel("Simple label", this);
-  }
+  QPushButton* button = new QPushButton(this);
+  button->setText("Click me");
+
+  QMenu* menu = new QMenu();
+  menu->addAction("Copy");
+  menu->addAction("Cut");
+  menu->addAction("Paste");
+  button->setMenu(menu);
+
+  QLineEdit* lineEdit = new QLineEdit(this);
+  lineEdit->setGeometry(105, 5, 250, 250);
+
+}
 
 MainWindow::~MainWindow() {}
